c733b1c7cf923f54f8222bb3d3e8d8a30bec5344
Curtis Aliwah <curtisaliwah5@gmail.com>
2024-04-16 01:01:49 +0300

    Created a card component with a progress bar component inside it using tremor

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Card, ProgressBar } from "@tremor/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className="text-3xl font-bold capitalize underline">hello world!</h1>
+      <div className="flex items-center justify-center h-screen">
+        <Card className="mx-auto max-w-md">
+          <h4 className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+            Sales
+          </h4>
+          <p className="text=tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
+            $71,389
+          </p>
+          <p className="mt-4 flex items-center justify-between text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+            <span>32% of annual target</span>
+            <span>$225,738</span>
+          </p>
+          <ProgressBar value={32} className="mt-2" />
+        </Card>
+      </div>
     </>
   );
 }
