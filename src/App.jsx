9c50841262b25e2fc2afc3bc17aec3f2de6e59e5
Curtis Aliwah <curtisaliwah5@gmail.com>
2024-04-16 14:13:05 +0300

    Made layout changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
-import { Card, ProgressBar } from "@tremor/react";
-
 function App() {
   return (
-    <>
-      <div className="flex items-center justify-center h-screen">
-        <Card className="mx-auto max-w-md">
-          <h4 className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
-            Sales
-          </h4>
-          <p className="text=tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
-            $71,389
-          </p>
-          <p className="mt-4 flex items-center justify-between text-tremor-default text-tremor-content dark:text-dark-tremor-content">
-            <span>32% of annual target</span>
-            <span>$225,738</span>
-          </p>
-          <ProgressBar value={32} className="mt-2" />
-        </Card>
+    <div className="grid grid-cols-2 gap-12">
+      <div>
+        <h2 className="text-2xl font-bold">Barbie</h2>
       </div>
-    </>
+      <div>
+        <h2 className="text-2xl font-bold">Oppenheimer</h2>
+      </div>
+    </div>
   );
 }
 
---
+++ src/Components/TestCard.jsx
@@ -0,0 +1,23 @@
+import { Card, ProgressBar } from "@tremor/react";
+
+const TestCard = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Card className="mx-auto max-w-md">
+        <h4 className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+          Sales
+        </h4>
+        <p className="text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
+          $71,389
+        </p>
+        <p className="mt-4 flex items-center justify-between text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+          <span>32% of annual target</span>
+          <span>$225,738</span>
+        </p>
+        <ProgressBar value={32} className="mt-2" />
+      </Card>
+    </div>
+  );
+};
+
+export default TestCard;
